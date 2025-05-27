4511bbade00ef7066155e53563221d7d85330d03
AND73321 <125512792+AND73321@users.noreply.github.com>
2025-05-27 10:46:32 -0500

    Delete API directory

--- API/src/main/java/com/servicios/API/repository/EstudianteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.servicios.API.repository;
-
-import com.servicios.API.entity.Estudiante;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstudianteRepository extends JpaRepository<Estudiante, String> {
-}
